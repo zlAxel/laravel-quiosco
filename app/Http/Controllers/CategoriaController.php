6a200f6698de63ba07c3dbc10d9d780aa2b358ad
zlAxel <axel.macias17@hotmail.com>
2023-09-16 23:25:36 -0600

    Se sube primer API (Categorias)

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CategoriaCollection;
+
 use App\Models\Categoria;
+
 use Illuminate\Http\Request;
 
 class CategoriaController extends Controller {
@@ -10,6 +13,8 @@ class CategoriaController extends Controller {
     * @return \Illuminate\Http\Response
     */
     public function index() {
-        return response()->json([ 'categorias' => Categoria::all() ]);
+        // return response()->json([ 'categorias' => Categoria::all() ]);
+
+        return new CategoriaCollection( Categoria::all() );
     }
 }
